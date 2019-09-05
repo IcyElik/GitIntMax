3fe6a074d1bfa0e1a0c6acda17943ec2b76c26dc
Elís W. E <elis18@ru.is>
2019-09-05 10:37:08 +0000

    Algorithm written for max_int

--- IntAlg.py
+++ IntAlg.py
@@ -2,6 +2,7 @@
 num_int = int(input("Input a number: "))    # Do not change this line
 max_int = None
 while num_int > 0:
+    num_int = int(input("Input a number: "))
     if max_int is None:
         max_int = num_int
     elif max_int < num_int:
